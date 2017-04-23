ec97f40b1b4c881a3f23209102df84daf1e9bef3
heisenberg99 <bhosle.ashwin95@gmail.com>
2017-04-23 00:47:25 -0700

    Updated Tags Page
    Added a back and next button

---
+++ CECS491Demo/app/src/main/java/edu/csulb/android/cecs491/American_Subtags.java
@@ -0,0 +1,13 @@
+package edu.csulb.android.cecs491;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class American_Subtags extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.american);
+    }
+}
